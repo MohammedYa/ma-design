a982bb986ac5302b8948c5fa75afbbfb3e73f587
MohammedYa <mohamedel3sawy4110@gmail.com>
2023-04-14 17:15:06 +0200

    add responsive home Style

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -8,23 +8,48 @@ declare var $ :any
 export class NavbarComponent implements OnInit {
   ngOnInit(): void {
     let currnetLeft=-91;
+    
     let Left=-230;
-    let windowsize = $(window).width();
 
 
-
-if (windowsize >390 &&windowsize<768) {
-  $("#list").click(function(){
+    $("#list").click(function(){
       $("#list").hide(1000);
       $("#sections").animate({"left":currnetLeft},1000);
-
+     
     })
-    $("#sections").click(function(){
+    $("#close").click(function(){  
+      $("#sections").animate({"left":Left },1000);
+
       $("#list").show(1000);
-      $(".links").animate({"left":Left },1000);
 
     })
-}
+
+    $(window).resize(function() {
+      let windowsize = $(window).width();
+      if(windowsize>=390&&windowsize<750){
+        $(".links").css({"left":Left });
+
+       
+      }
+      else  if (windowsize >=991&&windowsize<=1200 ) {
+          $(".links").css({"left":180 });
+
+      }
+      else  if (windowsize >=768&&windowsize<=990 ) {
+          $(".links").css({"left":95 });
+
+      }
+      else  if (windowsize >=1201 ) {
+        $(".links").css({"left":180 });
+
+    }
+   
+
+        
+        
+      
+     }); 
+
 
     
   }
